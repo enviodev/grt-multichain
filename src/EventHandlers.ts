2c43c342584c08a7ab63a9b9ff6c0e5d6fbb194b
denhampreen <denhampreen@gmail.com>
2023-12-04 08:25:36 +0200

    handle balance multichain

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -7,6 +7,15 @@ import {
 
 import { AccountEntity, ApprovalEntity } from "../generated/src/Types.gen";
 
+const polygonAddress = "0x5fe2B58c013d7601147DcdD68C143A77499f5531";
+const isPolygon = (srcAddress: string) => {
+  return srcAddress == polygonAddress;
+};
+const ethereumAddress = "0xc944E90C64B2c07662A292be6244BDf05Cda44a7";
+const isEthereum = (srcAddress: string) => {
+  return srcAddress == ethereumAddress;
+};
+
 ERC20Contract_Approval_loader(({ event, context }) => {
   // loading the required Account entity
   context.Account.load(event.params.owner.toString());
@@ -23,6 +32,8 @@ ERC20Contract_Approval_handler(({ event, context }) => {
     let accountObject: AccountEntity = {
       id: event.params.owner.toString(),
       balance: 0n,
+      balancePolygon: 0n,
+      balanceEthereum: 0n,
     };
     context.Account.set(accountObject);
   }
@@ -52,17 +63,36 @@ ERC20Contract_Transfer_handler(({ event, context }) => {
   if (senderAccount === undefined || senderAccount === null) {
     // create the account
     // This is likely only ever going to be the zero address in the case of the first mint
+
+    let balancePolygon = isPolygon(event.srcAddress)
+      ? 0n - event.params.value
+      : 0n;
+    let balanceEthereum = isEthereum(event.srcAddress)
+      ? 0n - event.params.value
+      : 0n;
+
     let accountObject: AccountEntity = {
       id: event.params.from.toString(),
       balance: 0n - event.params.value,
+      balancePolygon: balancePolygon,
+      balanceEthereum: balanceEthereum,
     };
 
     context.Account.set(accountObject);
   } else {
+    let balancePolygon = isPolygon(event.srcAddress)
+      ? senderAccount.balancePolygon - event.params.value
+      : senderAccount.balancePolygon;
+    let balanceEthereum = isEthereum(event.srcAddress)
+      ? senderAccount.balanceEthereum - event.params.value
+      : senderAccount.balanceEthereum;
+
     // subtract the balance from the existing users balance
     let accountObject: AccountEntity = {
       id: senderAccount.id,
       balance: senderAccount.balance - event.params.value,
+      balancePolygon: balancePolygon,
+      balanceEthereum: balanceEthereum,
     };
     context.Account.set(accountObject);
   }
@@ -71,16 +101,34 @@ ERC20Contract_Transfer_handler(({ event, context }) => {
 
   if (receiverAccount === undefined || receiverAccount === null) {
     // create new account
+
+    let balancePolygon = isPolygon(event.srcAddress) ? event.params.value : 0n;
+    let balanceEthereum = isEthereum(event.srcAddress)
+      ? event.params.value
+      : 0n;
+
     let accountObject: AccountEntity = {
       id: event.params.to.toString(),
       balance: event.params.value,
+      balancePolygon: balancePolygon,
+      balanceEthereum: balanceEthereum,
     };
     context.Account.set(accountObject);
   } else {
     // update existing account
+
+    let balancePolygon = isPolygon(event.srcAddress)
+      ? receiverAccount.balancePolygon + event.params.value
+      : receiverAccount.balancePolygon;
+    let balanceEthereum = isEthereum(event.srcAddress)
+      ? receiverAccount.balanceEthereum + event.params.value
+      : receiverAccount.balanceEthereum;
+
     let accountObject: AccountEntity = {
       id: receiverAccount.id,
       balance: receiverAccount.balance + event.params.value,
+      balancePolygon: balancePolygon,
+      balanceEthereum: balanceEthereum,
     };
 
     context.Account.set(accountObject);
